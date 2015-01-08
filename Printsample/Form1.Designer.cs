e4f9c73c12c93ab5796aedafb9103ec2b59a8213
aelnagase <nagase@aelnet.co.jp>
2015-01-08 14:40:09 +0900

    2nd commt by nagase

--- Printsample/Form1.Designer.cs
+++ Printsample/Form1.Designer.cs
@@ -28,25 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPrinterListUp = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.btnPrinTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnPrinterListUp
             // 
-            this.button1.Location = new System.Drawing.Point(74, 27);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnPrinterListUp.Location = new System.Drawing.Point(63, 40);
+            this.btnPrinterListUp.Name = "btnPrinterListUp";
+            this.btnPrinterListUp.Size = new System.Drawing.Size(150, 23);
+            this.btnPrinterListUp.TabIndex = 0;
+            this.btnPrinterListUp.Text = "接続プリンター一覧";
+            this.btnPrinterListUp.UseVisualStyleBackColor = true;
+            this.btnPrinterListUp.Click += new System.EventHandler(this.btnPrinterListUp_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // btnPrinTest
+            // 
+            this.btnPrinTest.Location = new System.Drawing.Point(67, 120);
+            this.btnPrinTest.Name = "btnPrinTest";
+            this.btnPrinTest.Size = new System.Drawing.Size(150, 23);
+            this.btnPrinTest.TabIndex = 1;
+            this.btnPrinTest.Text = "印刷テスト";
+            this.btnPrinTest.UseVisualStyleBackColor = true;
+            this.btnPrinTest.Click += new System.EventHandler(this.btnPrinTest_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPrinTest);
+            this.Controls.Add(this.btnPrinterListUp);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +84,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPrinterListUp;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Button btnPrinTest;
+
     }
 }
 
--- Printsample/Form1.cs
+++ Printsample/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+
+using System.Drawing.Printing;
 
 namespace Printsample
 {
@@ -17,9 +20,183 @@ namespace Printsample
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private string sTargetPrinterName = "";
+
+
+        // 接続プリンターの一覧
+        private void btnPrinterListUp_Click(object sender, EventArgs e)
         {
+            foreach (string sPrinerName in PrinterSettings.InstalledPrinters)
+            {
+                Debug.Print(sPrinerName);
+                if (sPrinerName.IndexOf("LBP9600C") >= 0)
+                {
+                    sTargetPrinterName = sPrinerName;
+                }
+            }
+        }
+
+
+        private void initializePrinterSetting1(string sPrinterName)
+        {
+            //プリンター名の設定
+            if (sPrinterName != "")
+            {
+                this.printDocument1.PrinterSettings.PrinterName = sPrinterName;
+                if (this.printDocument1.PrinterSettings.IsValid == false)
+                {
+                    //プリンター名異常
+                    throw new Exception("プリンター名異常");
+                }
+            }
+
+            // A4サイズを指定
+            int nPaperIndex = 0;
+            foreach (PaperSize ps in printDocument1.PrinterSettings.PaperSizes)
+            {
+                Debug.WriteLine(ps.PaperName);
+                if (ps.PaperName.IndexOf("A4") > -1)
+                {
+                    break;
+                }
+                nPaperIndex++;
+            }
+
+            // 両面印刷可能のプリンター
+            if (printDocument1.PrinterSettings.CanDuplex == true)
+            {
+                printDocument1.PrinterSettings.Duplex = Duplex.Simplex;　　//　片面印刷に変更
+            }
+
+            printDocument1.DefaultPageSettings.PaperSize = printDocument1.PrinterSettings.PaperSizes[nPaperIndex];
+            // printDocument1.DefaultPageSettings.Landscape = true;    // 横向き
+            printDocument1.DefaultPageSettings.Landscape = false;    // 縦向き
+            //printDocument1.DefaultPageSettings.Color = true;        // カラー印刷
+        }
+
+        private void btnPrinTest_Click(object sender, EventArgs e)
+        {
+            initializePrinterSetting1(sTargetPrinterName);
+   
+            // プレビュー表示
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+
+            //　印刷
+            // printDocument1.Print();
 
         }
+
+        private printItem[] prtData1 = {
+                new printItem(0.3F,39,28,100,37,"名前１",10,0,"name1"),
+                new printItem(0F,39,37,100,48,"名前２",10,0,"name2"),
+        };
+
+        private void printDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            //　扱いやすい様に mm 単位スケールに変更
+            e.Graphics.PageUnit = GraphicsUnit.Millimeter;
+
+            float xMargn = -28;
+            float yMargn = -5;
+
+            for (int i = 0; i < prtData1.Length; i++)
+            {
+                Pen objPen;
+                Font objFont;
+                RectangleF oRect;
+                StringFormat oFormat = new StringFormat();
+                oFormat.Alignment = StringAlignment.Near;
+                oFormat.LineAlignment = StringAlignment.Center;
+
+                if (prtData1[i].PenWidth != 0)
+                {
+                    objPen = new Pen(Color.Black, (float)prtData1[i].PenWidth);
+                    e.Graphics.DrawRectangle(objPen, prtData1[i].x + xMargn, prtData1[i].y + yMargn, prtData1[i].width, prtData1[i].height);
+                }
+
+                if (prtData1[i].sMessage != "")
+                {
+                    switch (prtData1[i].AlignMent)
+                    {
+                        case 0:
+                            oFormat.Alignment = StringAlignment.Near;
+                            break;
+                        case 1:
+                            oFormat.Alignment = StringAlignment.Center;
+                            break;
+                        case 2:
+                            oFormat.Alignment = StringAlignment.Far;
+                            break;
+                    }
+
+                    objFont = new Font("MS UI Gothic", prtData1[i].FontSize);
+                    oRect = new RectangleF(prtData1[i].x + xMargn, prtData1[i].y + yMargn, prtData1[i].width, prtData1[i].height);
+                    e.Graphics.DrawString(prtData1[i].sMessage, objFont, Brushes.Black, oRect, oFormat);
+                }
+            }
+
+            // 多角形の描画
+            { 
+                int sx = 30;
+                int sy = 60;
+                int x1 = sx + 40;
+                int x2 = sx + 80;
+                int y1 = sy + 20;
+                int y2 = sy + 60;
+                int y3 = sy + 80;
+
+                Point[] oPoints = {   
+                    new Point(x1, sy),
+                    new Point(sx, y1),
+                    new Point(sx, y2),
+                    new Point(x1, y3),
+                    new Point(x2, y2),
+                    new Point(x2, y1)
+                };
+                Pen objPen = new Pen(Color.Red, (float)0.3);
+                e.Graphics.DrawPolygon(objPen, oPoints);
+
+            }
+
+
+
+            e.HasMorePages = false;
+        }
+
+       
+    
+    }
+
+    public class printItem
+    {
+        public float PenWidth;
+        public float x;
+        public float y;
+        public float width;
+        public float height;
+        public String sMessage;
+        public int FontSize;
+        public int AlignMent;
+        public string sLabel;
+
+        public printItem(float _PenWidth, float StartX, float StartY, float EndX, float EndY, String _sMessage, int _FontSize, int _AlignMent)
+            : this(_PenWidth, StartX, StartY, EndX, EndY, _sMessage, _FontSize, _AlignMent, "")
+        {
+        }
+
+        public printItem(float _PenWidth, float StartX, float StartY, float EndX, float EndY, String _sMessage, int _FontSize, int _AlignMent, string _sLabel)
+        {
+            PenWidth = _PenWidth;
+            x = StartX;
+            y = StartY;
+            width = EndX - StartX;
+            height = EndY - StartY;
+            sMessage = _sMessage;
+            FontSize = _FontSize;
+            AlignMent = _AlignMent;
+            sLabel = _sLabel;
+        }
+
     }
 }
